e0562c4101763581883f70fc3ac71fe735f8eea4
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-02-12 00:06:43 +0600

    backend subcategory

---
+++ backend/controllers/allSubCategoryController.js
@@ -0,0 +1,11 @@
+const subCategory = require("../model/subCategorySchema");
+
+let allSubCategoryController = async (req, res) => {
+
+    let categoryData = await subCategory.find({}).populate("subCategoryId");
+    res.send(categoryData);
+
+    console.log(categoryData);
+};
+
+module.exports = allSubCategoryController;
--- backend/routes/api/productRoutes.js
+++ backend/routes/api/productRoutes.js
@@ -3,11 +3,16 @@ const _ = express.Router();
 const categoryController = require('../../controllers/categoryController');
 const category = require("../../model/categorySchema")
 
+const subCategoryController = require("../../controllers/subCategoryController");
+const allsubCategoryController = require("../../controllers/allSubCategoryController");
+
 _.post("/createcategory", categoryController);
 _.get("/allcreatecategory", async function (req,res){
 let data = await category.find({}).populate("ownerId")
 res.send(data)
 });
 
+_.post("/subcategory", subCategoryController);
+_.get("/allsubcategory", allsubCategoryController);
 
 module.exports = _;
