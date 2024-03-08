a2c7e3c304b55bd8ce845c29ee2a34ff03ef7157
Amit Roy <119528393+amitroy4@users.noreply.github.com>
2024-03-09 02:13:06 +0600

    product varience done

--- dashboard/src/pages/AddProduct.jsx
+++ dashboard/src/pages/AddProduct.jsx
@@ -3,13 +3,15 @@ import { Button, Checkbox, Form, Input, Card } from "antd";
 
 const AddProduct = () => {
   let [variantvalue, setVariantvalue] = useState([]);
+  let [value, setValue] = useState("");
+  let [valueStock, setValueStock] = useState("");
   const onFinish = (values) => {
     console.log("Success:", values);
     let arr = [...variantvalue];
 
     arr.push({
       name: values.variantname,
-      value: values.variantvalue,
+      value: [],
     });
     setVariantvalue(arr);
   };
@@ -17,15 +19,14 @@ const AddProduct = () => {
     console.log("Failed:", errorInfo);
   };
 
-//   let handleVariant = () => {
-//     let arr = [...variantvalue];
-
-//     arr.push({
-//       name: values.variantname,
-//       value: values.variantvalue,
-//     });
-//     setVariantvalue(arr);
-//   };
+  let handleVariantValue = (index) => {
+    variantvalue[index].value.push({
+      name: value,
+      stock: valueStock,
+    });
+    let arr = [...variantvalue];
+    setVariantvalue(arr);
+  };
 
   return (
     <>
@@ -59,7 +60,6 @@ const AddProduct = () => {
         >
           <Input />
         </Form.Item>
-
         <Form.Item
           label="Description"
           name="description"
@@ -74,6 +74,35 @@ const AddProduct = () => {
         </Form.Item>
 
         <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
           label="Variant Name"
           name="variantname"
           rules={[
@@ -85,24 +114,6 @@ const AddProduct = () => {
         >
           <Input />
         </Form.Item>
-
-        <Form.Item
-          label="Variant Value"
-          name="variantvalue"
-          rules={[
-            {
-              required: true,
-              message: "Please input your variant!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Button  type="primary">
-          Add Variant
-        </Button>
-
         <Form.Item
           wrapperCol={{
             offset: 8,
@@ -110,34 +121,40 @@ const AddProduct = () => {
           }}
         >
           <Button type="primary" htmlType="submit">
-            Submit
+            Add Variant
           </Button>
         </Form.Item>
         {variantvalue.length > 0 && (
-          <Card
-            style={{
-              width: 300,
-            }}
-          >
-            {variantvalue.map((item) => (
-              <>
-                <div>{item.name}</div>
-                <div>{item.value}</div>
-              </>
-              //   <Form.Item
-              //     label="Variant Name"
-              //     name="variantname"
-              //     rules={[
-              //       {
-              //         required: true,
-              //         message: "Please input your variant!",
-              //       },
-              //     ]}
-              //   >
-              //     <Input />
-              //   </Form.Item>
+          <>
+            {variantvalue.map((item, index) => (
+              <Card
+                style={{
+                  width: 300,
+                }}
+              >
+                <>
+                  <div key={index}>
+                    <b>{item.name}</b>
+                  </div>
+                  <input
+                    placeholder="Value name"
+                    onChange={(e) => setValue(e.target.value)}
+                  />
+                  <input
+                    placeholder="Stock"
+                    onChange={(e) => setValueStock(e.target.value)}
+                  />
+                  <Button onClick={() => handleVariantValue(index)}>Add</Button>
+                  {item.value.map((i) => (
+                    <>
+                      <p>{i.name}</p>
+                      <p>{i.stock}</p>
+                    </>
+                  ))}
+                </>
+              </Card>
             ))}
-          </Card>
+          </>
         )}
       </Form>
     </>
